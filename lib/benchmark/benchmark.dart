bb1689c3c9739d039534e34677c1723d58028462
Xoldarov Temur <temuruz2003@gmail.com>
2024-07-09 22:49:55 +0500

    benchmark base64 30mb

---
+++ lib/benchmark/benchmark.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:isolate';
+
+///Test file [encoding,decoding] in base64 format
+void main() async {
+  final start = DateTime.now().millisecondsSinceEpoch;
+  final file = File(
+      "C:/Users/User/AndroidStudioProjects/cancellable_requester/safe_file_sender/test/input.exe");
+  final output = File(
+      "C:/Users/User/AndroidStudioProjects/cancellable_requester/safe_file_sender/test/output.exe");
+  final base64 = await Isolate.run(
+      () => Future.value(base64Encode(file.readAsBytesSync())));
+  await output.writeAsBytes(base64Decode(base64));
+  final end = DateTime.now().millisecondsSinceEpoch;
+  print("Benchmark : ${(end - start)} ms");
+}
