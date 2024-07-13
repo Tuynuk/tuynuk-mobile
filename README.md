10159ef483fb63ac191ee499bed9d4be10bfe0e9
Xoldarov Temur <temuruz2003@gmail.com>
2024-07-13 14:47:35 +0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ![alt text](https://lokdon.com/wp-content/uploads/2021/09/Subtitle_01-2.jpg)
+![alt text](https://github.com/xaldarof/tuynuk/blob/main/assets/images/logo_dark.png)
--- lib/benchmark/benchmark.dart
+++ lib/benchmark/benchmark.dart
@@ -7,14 +7,11 @@ import '../crypto/crypto.dart';
 ///Test file [encoding,decoding] in base64 format
 void main() async {
   final start = DateTime.now().millisecondsSinceEpoch;
-  final file = File(
-      "C:/Users/User/AndroidStudioProjects/cancellable_requester/safe_file_sender/files/input.exe");
-  final output = File(
-      "C:/Users/User/AndroidStudioProjects/cancellable_requester/safe_file_sender/files/output.exe");
+  final file = File("${Directory.current.path}/files/input.exe");
+  final output = File("${Directory.current.path}/files/output.exe");
   final base64 = await Isolate.run(
       () => Future.value(base64Encode(file.readAsBytesSync())));
   await output.writeAsBytes(base64Decode(base64));
   final end = DateTime.now().millisecondsSinceEpoch;
   print("Benchmark : ${(end - start)} ms");
 }
-
