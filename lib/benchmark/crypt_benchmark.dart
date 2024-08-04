182e609c6b7b05f2feb52c2e50dc322dba71373f
Temur Home PC <temuruz2003@gmail.com>
2024-08-04 23:58:06 +0500

    add benchmarkTimer class for benchmarking

--- lib/benchmark/crypt_benchmark.dart
+++ lib/benchmark/crypt_benchmark.dart
@@ -1,20 +1,56 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
-import 'package:convert/convert.dart';
 import 'package:safe_file_sender/crypto/crypto.dart';
 
-void main() {
-  final start = DateTime.now().millisecondsSinceEpoch;
-  final userA = AppCrypto.generateECKeyPair();
-  final userB = AppCrypto.generateECKeyPair();
-  final sharedKey =
-      AppCrypto.deriveSharedSecret(userA.privateKey, userB.publicKey);
+final _file = File("${Directory.current.path}/files/input.exe");
+late Uint8List _testKey;
 
-  final file = File("${Directory.current.path}/files/input.exe");
-  final endDeriveSharedKey = DateTime.now().millisecondsSinceEpoch;
-  print("Benchmark derive shared key: ${(endDeriveSharedKey - start)}ms");
-  final hmac = AppCrypto.generateHMAC(sharedKey, file.readAsBytesSync());
-  print("HMAC ${hex.encode(hmac)}");
-  final end = DateTime.now().millisecondsSinceEpoch;
-  print("Benchmark generate HMAC: ${(end - start)}ms");
+void main() {
+  _testKey = base64Decode(
+      AppCrypto.encodeECPublicKey(AppCrypto.generateECKeyPair().publicKey));
+  _benchmarkSharedKey();
+  _benchmarkEncryption();
+  _benchmarkHmacGeneration();
+}
+
+void _benchmarkSharedKey() {
+  final benchmark = BenchmarkTimer("share key derive");
+
+  benchmark.run(() {
+    final userA = AppCrypto.generateECKeyPair();
+    final userB = AppCrypto.generateECKeyPair();
+    AppCrypto.deriveSharedSecret(userA.privateKey, userB.publicKey);
+  });
+}
+
+void _benchmarkHmacGeneration() {
+  final benchmark = BenchmarkTimer("hmac generation");
+
+  benchmark.run(() {
+    AppCrypto.generateHMAC(_testKey, _file.readAsBytesSync());
+  });
+}
+
+void _benchmarkEncryption() {
+  final benchmark = BenchmarkTimer("file encryption");
+  benchmark.run(() {
+    AppCrypto.encryptAES(_file.readAsBytesSync(), _testKey);
+  });
+}
+
+class BenchmarkTimer {
+  final String _name;
+
+  int run(Function() action) {
+    final start = DateTime.now().millisecondsSinceEpoch;
+    action.call();
+    final end = DateTime.now().millisecondsSinceEpoch;
+    final result = end - start;
+    print("Benchmark for ($_name): $result ms");
+    return result;
+  }
+
+  BenchmarkTimer(this._name);
 }
