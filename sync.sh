55ce00096405afa9f54b6ecfbc1d55d1e5fce0f4
Temur <69597351+xaldarof@users.noreply.github.com>
2024-08-09 00:40:05 +0500

    fix naming

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the .env file name
-ENV_FILE="env.env"
+ENV_FILE="environment.env"
 
 # Generate a key in base64 format
 KEY=$(openssl rand -base64 12)
@@ -10,11 +10,9 @@ KEY=$(openssl rand -base64 12)
 if [ ! -f "$ENV_FILE" ]; then
     touch $ENV_FILE
     echo "key='$KEY'" >> $ENV_FILE
-    echo ".env file created with a 16-byte key."
+    echo ".env file created with a key."
 else
     echo "env.env file already exists."
-#    echo "KEY=$KEY" >> $ENV_FILE
-#    echo "16-byte key added to the existing .env file."
 fi
 
 dart run build_runner build
